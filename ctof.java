801a5c5af665a54bb5203944265ce7abd6e0539e
Shashank <shashankhn111@gmail.com>
2025-04-08 11:21:52 -0700

    basic

---
+++ ctof.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ctof {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        float c=scan.nextFloat();
+        float f=(c*9/5)+32;
+        System.out.println(f);
+    }
+    
+}
